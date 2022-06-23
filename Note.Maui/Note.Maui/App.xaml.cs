096cb228304a0edc7024b6bd18454a5f0396c1f4
Pedro Gonzalez <prgazure@gmail.com>
2022-06-23 18:47:50 -0300

    Primer Proyecto

---
+++ Note.Maui/Note.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Note.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Note.Maui/Note.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Note.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

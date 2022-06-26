0a4859e8bec17dc537e6600f066205660869d45b
Pedro Gonzalez <prgazure@gmail.com>
2022-06-26 00:07:06 -0300

    agregando codigo en MainPage.xaml.cs

--- Note.Maui/Note.Maui/MainPage.xaml.cs
+++ Note.Maui/Note.Maui/MainPage.xaml.cs
@@ -3,22 +3,62 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	private string _fileName;
 
 	public MainPage()
 	{
-		InitializeComponent();
+		var notesHeading = new Label()
+		{
+			Text = "Notes",
+			HorizontalOptions = LayoutOptions.Center,
+			FontAttributes = FontAttributes.Bold
+		};
+
+		var editor = new Editor()
+		{
+			Placeholder="Ingrese su nota", HeightRequest = 100
+		};
+		editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
+		
+		var buttonsGrid = new Grid() { HeightRequest = 40.0};
+		buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+		buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
+		buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+
+		var saveButton = new Button() { WidthRequest = 100, Text = "Guardar" };
+		saveButton.HorizontalOptions = LayoutOptions.Center;
+		saveButton.Clicked += SaveButton_Clicked;
+		buttonsGrid.Add(saveButton);
+
+		Grid.SetColumn(saveButton, 0);
+
+		var deleteButton = new Button() { WidthRequest = 100, Text = "Borrar" };
+		deleteButton.HorizontalOptions = LayoutOptions.Center;
+		deleteButton.Clicked += DeleteButton_Clicked;
+		Grid.SetColumn(deleteButton, 2);
+
+		buttonsGrid.Children.Add(deleteButton);
+
+		var stackLoyout = new VerticalStackLayout
+		{
+			Padding = new Thickness(30, 60, 30, 30),
+			Children = { notesHeading, editor, buttonsGrid }
+		};
+
+		Content = stackLoyout;
+
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void DeleteButton_Clicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		throw new NotImplementedException();
 	}
+
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		throw new NotImplementedException();
+	}
+
+	
 }
 

551b1f47b96f1fbcebe263e9c8c3f0c978c5d5b6
Pedro Gonzalez <prgazure@gmail.com>
2022-06-28 23:29:27 -0300

    Agregando calses staticas para customizar la vista

---
+++ Note.Maui/Note.Maui/NuevaPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace Note.Maui
+{    
+    public partial class NuevaPage : ContentPage
+    {
+        string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+        public const double MyFontSize = 28;
+        public NuevaPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class GlobalFontSizeExtension : IMarkupExtension
+    {
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return NuevaPage.MyFontSize;
+        }
+    }
+}
